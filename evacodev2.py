edab94bc46f38a16a6f3d4d243bd672a97b1ddf4
Dawijak04 <145558391+Dawijak04@users.noreply.github.com>
2026-09-15 13:35:08 +0100

    Select country

--- evacodev2.py
+++ evacodev2.py
@@ -7,33 +7,48 @@ with open("eva-data.json", "r", encoding="utf-8") as file:
 
 records = []
 
+print("Please input the country you want to see data for: ")
+COUNTRY = input()
+
+def check_country(record):
+    country = record[2]
+    return country == COUNTRY
+
 for eva in eva_data:
     date_text = eva.get("date")
     duration_text = eva.get("duration")
+    country_text = eva.get("country")
 
-    if not date_text or not duration_text:
+    if not date_text or not duration_text or not country_text:
         continue
 
     date = datetime.fromisoformat(date_text)
     hours, minutes = map(int, duration_text.split(":"))
     duration_hours = hours + minutes / 60
 
-    records.append((date, duration_hours))
+    records.append((date, duration_hours, country_text))
 
 records.sort(key=lambda record: record[0])
+records_country = filter(check_country, records)
 
-dates = []
-cumulative_hours = []
-total_hours = 0
 
-for date, duration_hours in records:
-    total_hours += duration_hours
-    dates.append(date)
-    cumulative_hours.append(total_hours)
+def cumulate_hour_and_plot(plot_records, country="all countries"):
+    dates = []
+    cumulative_hours = []
+    total_hours = 0
 
-plt.plot(dates, cumulative_hours)
-plt.xlabel("Year")
-plt.ylabel("Cumulative EVA duration (hours)")
-plt.tight_layout()
-plt.savefig("cumulative_duration.png")
-plt.show()
+    for date, duration_hours, country_text in plot_records:
+        total_hours += duration_hours
+        dates.append(date)
+        cumulative_hours.append(total_hours)
+
+    plt.plot(dates, cumulative_hours)
+    plt.title("Cumulative hours for " + country)
+    plt.xlabel("Year")
+    plt.ylabel("Cumulative EVA duration (hours)")
+    plt.tight_layout()
+    plt.savefig(f"cumulative_duration_{COUNTRY}.png")
+    plt.show()
+
+cumulate_hour_and_plot(records)
+cumulate_hour_and_plot(records_country, COUNTRY)
